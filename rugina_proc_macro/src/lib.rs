cb8ae147d249a3780050d7e0cf56293d1324cc73
Potochi <budliviu@gmail.com>
2022-12-12 16:50:41 +0200

    Fa limbajul rugina sa fie foarte sigur

--- rugina_proc_macro/src/lib.rs
+++ rugina_proc_macro/src/lib.rs
@@ -26,7 +26,7 @@ fn replace_ident(ident: Ident) -> Option<TokenTree> {
     "ca" => "as",
     "constant" => "const",
     "trăsătură" => "trait",
-    "nesigur" => "unsafe",
+    "foarte_sigur" => "unsafe",
     "în" => "in",
     "din" => "from",
     "dinamic" => "dyn",
